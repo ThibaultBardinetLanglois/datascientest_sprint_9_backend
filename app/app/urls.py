626b7d589c95cb8c2d7aaba6d40fdb877979d84c
Thibault Bardinet-Langlois <langlois.bardinet.thibault@gmail.com>
2025-09-04 14:28:18 +0200

    add health "/"

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("articles/", include("articles.urls")),
-    path("health", include("health.urls"))
+    path("health/", include("health.urls"))
 ]
